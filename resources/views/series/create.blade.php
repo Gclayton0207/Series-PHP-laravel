d743c93d826e5f484c560269f1a4971d369eae1d
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-28 22:35:43 -0300

    feat: integrando banco de dados

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="Nova Serie">
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome">
